724f5ef7c34ad0b28c7f98633f605f26beff391e
Zhdanov <vzhdanov@byui.edu>
2021-11-19 14:25:44 -0700

    set up a simple server file

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import http from 'http';
+import config from 'config';
+
+const app = express();
+
+const PORT = process.env.PORT || config.get('PORT');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+    next();
+});
+
+app.set('port'.PORT);
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => console.log(`App is running on port ${PORT}`));
